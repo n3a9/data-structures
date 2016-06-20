bfc3ea886f8870c525d844a8dfa566236ab6701e
lifegood141 <aggarwal.neeraj141@gmail.com>
2016-06-20 13:54:56 -0700

    Add queue base implementation, streamline stack

--- test.js
+++
@@ -1,8 +0,0 @@
-var q = new Queue();
-q.enqueue("hello");
-q.enqueue("user");
-console.log(q.getSize());
-console.log(q.peek());
-console.log(q.dequeue());
-console.log(q.getSize());
-console.log(q.peek());
